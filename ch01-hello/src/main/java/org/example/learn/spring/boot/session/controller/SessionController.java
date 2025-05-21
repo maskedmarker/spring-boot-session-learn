909b7e380b1e7f1a7fb877a1e1327d1d61b8b1c1
maskedmarker <changjianghui0822@gmail.com>
2025-05-21 23:17:24 +0800

    no msg

--- ch01-hello/src/main/java/org/example/learn/spring/boot/session/controller/SessionController.java
+++ ch01-hello/src/main/java/org/example/learn/spring/boot/session/controller/SessionController.java
@@ -3,16 +3,27 @@ package org.example.learn.spring.boot.session.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/session")
 public class SessionController {
 
     @RequestMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
         Date now = Calendar.getInstance().getTime();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("timestamp", now.toString());
+        data.put("name", "zhangsan");
+
+        request.getSession().setAttribute("data", data);
+
+
         return "hello world" + " at " + now.toGMTString();
     }
 }
